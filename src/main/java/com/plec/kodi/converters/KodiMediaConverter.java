28a9bac2451cb4edd6b49aa053f5e1b89116db9a
plec <pierreleclainche@neuf.fr>
2018-06-04 17:44:19 +0200

    change extract image for movie to get only a poster image

--- src/main/java/com/plec/kodi/converters/KodiMediaConverter.java
+++ src/main/java/com/plec/kodi/converters/KodiMediaConverter.java
@@ -17,48 +17,36 @@ public class KodiMediaConverter {
 	private static final Logger LOGGER = LoggerFactory.getLogger(KodiMediaConverter.class);
 
 
-	private static final String PATTERN_START_IMAGE_MOVIE = "preview=\"";
-	private static final String PATTERN_END_IMAGE_MOVIE = "\">";
-
-	private static final String PATTERN_START_IMAGE_TVSHOW = "<thumb aspect=\"poster\"";
-	private static final String PATTERN_START_IMAGE_TVSHOW_URL = ">";
-	private static final String PATTERN_END_IMAGE_TVSHOW = "</thumb>";
+	private static final String PATTERN_START_XML_IMAGE = "<thumb aspect=\"poster\"";
+	private static final String PATTERN_START_XML_IMAGE_URL = ">";
+	private static final String PATTERN_END_XML_IMAGE = "</thumb>";
 	
 	
 	public static KodiMedia convert(MovieEntity me) {
-		return new KodiMedia(me.getTitle(), me.getResume(), extractMovieImageUrl(me.getImage()), Arrays.stream(me.getGenre().split("/")).map(s -> s.trim()).collect(Collectors.toList()), me.getOriginal_title());
+		return new KodiMedia(me.getTitle(), me.getResume(), extractImageUrl(me.getImage()), Arrays.stream(me.getGenre().split("/")).map(s -> s.trim()).collect(Collectors.toList()), me.getOriginal_title());
 	}
 	public static KodiMedia convert(TvShowEntity tse) {
-		return new KodiMedia(tse.getTitle(), tse.getResume(), extractTvShowImageUrl(tse.getImage()), Arrays.stream(tse.getGenre().split("/")).map(s -> s.trim()).collect(Collectors.toList()), tse.getOriginal_title());
+		return new KodiMedia(tse.getTitle(), tse.getResume(), extractImageUrl(tse.getImage()), Arrays.stream(tse.getGenre().split("/")).map(s -> s.trim()).collect(Collectors.toList()), tse.getOriginal_title());
 	}
 	public static Genre convert(GenreEntity ge) {
 		return new Genre(ge.getId(), ge.getValue());
 	}
 	
-	private static String extractMovieImageUrl(String xmlImage) {
-		int startFirstImageUrl = StringUtils.indexOf(xmlImage, PATTERN_START_IMAGE_MOVIE);
-		int endFirstImageUrl = StringUtils.indexOf(xmlImage, PATTERN_END_IMAGE_MOVIE);
-		int startIndex = startFirstImageUrl + PATTERN_START_IMAGE_MOVIE.length();
-		String imageUrl = new String(StringUtils.substring(xmlImage, startIndex, endFirstImageUrl));
-		if (!StringUtils.startsWith(imageUrl, "https://")) {
-			imageUrl = StringUtils.replace(imageUrl, "http://", "https://");
-		}
-		return imageUrl;
-	}
-	private static String extractTvShowImageUrl(String xmlImage) {
+
+	private static String extractImageUrl(String xmlImage) {
 		//find an image of type poster
-		int startFirstImageUrl = StringUtils.indexOf(xmlImage, PATTERN_START_IMAGE_TVSHOW);
-		int startIndex = startFirstImageUrl + PATTERN_START_IMAGE_TVSHOW.length();
+		int startFirstImageUrl = StringUtils.indexOf(xmlImage, PATTERN_START_XML_IMAGE);
+		int startIndex = startFirstImageUrl + PATTERN_START_XML_IMAGE.length();
 		String posterImageUrl = new String(StringUtils.substring(xmlImage, startIndex));
 		LOGGER.debug("tvshow poster URL step 1 "+ posterImageUrl);
 		
 		//remove the starting string with before the URL
-		int startUrl = StringUtils.indexOf(posterImageUrl, PATTERN_START_IMAGE_TVSHOW_URL);
-		posterImageUrl = new String(StringUtils.substring(posterImageUrl, startUrl + PATTERN_START_IMAGE_TVSHOW_URL.length()));
+		int startUrl = StringUtils.indexOf(posterImageUrl, PATTERN_START_XML_IMAGE_URL);
+		posterImageUrl = new String(StringUtils.substring(posterImageUrl, startUrl + PATTERN_START_XML_IMAGE_URL.length()));
 		LOGGER.debug("tvshow poster URL step 2 "+ posterImageUrl);
 		
 		//remove end string starts with </thumb>
-		int endImageUrl = StringUtils.indexOf(posterImageUrl, PATTERN_END_IMAGE_TVSHOW);
+		int endImageUrl = StringUtils.indexOf(posterImageUrl, PATTERN_END_XML_IMAGE);
 		posterImageUrl = new String (StringUtils.substring(posterImageUrl, 0, endImageUrl));
 		LOGGER.debug("tvshow poster URL step 3 "+ posterImageUrl);
 		
